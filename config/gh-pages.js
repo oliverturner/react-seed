f8286f543f6c1db3f7c519e820d6b1fae01eae4d
Oliver Turner <oliver.turner@codedsignal.co.uk>
2016-07-06 15:15:08 +0100

    removed redundant loaders

--- config/gh-pages.js
+++ config/gh-pages.js
@@ -1,7 +1,7 @@
 const ghpages = require('gh-pages')
 const path    = require('path')
 
-ghpages.publish(path.join(__dirname, '..', 'dist'), function(err) {
+ghpages.publish(path.join(__dirname, '..', 'dist'), function (err) {
   if (err) return console.log(err)
 
   return console.log('published to Github Pages')
